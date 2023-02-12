610740608324f9a98d5dc2aca147a179e8b5a9b3
puranchandralohar <clpuran95@gmail.com>
2023-02-12 14:09:01 +0530

    music added

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const clock = document.querySelector('.bar_clock');
+const clock = document.querySelector(".bar_clock");
+const audio = document.querySelector("audio");
 
-
+const playBtn = document.querySelector(".play_pause");
 
 // Clock
-const getTime = setInterval(()=>{
-    let time = new Date().toLocaleTimeString();
-    clock.innerText = time;
-},1000)
+const getTime = setInterval(() => {
+  let time = new Date().toLocaleTimeString();
+  clock.innerText = time;
+}, 1000);
+
+
+let isPlaying =false;
+
+const playSong = () => {
+    isPlaying = true;
+    playBtn.innerHTML = '<i class="fa-solid fa-pause">'
+    audio.play();
+}
+
+const pauseSong = () => {
+    isPlaying = false;
+    playBtn.innerHTML = '<i class="fa-solid fa-play">'
+    audio.pause();
+}
+
+playBtn.addEventListener('click', () => {
+    isPlaying ? pauseSong() : playSong();
+})
 
